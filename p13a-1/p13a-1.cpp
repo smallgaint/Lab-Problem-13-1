a8aa83b3f2688a0705e63c1f6ace68fb9956e24b
s-hong.1 <s-hong.1@onu.edu>
2025-12-04 12:24:49 -0500

    first

---
+++ p13a-1/p13a-1.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+
+using namespace std;
+
+void pressEnterToContinue();
+bool readFromFile(string filename);
+int sumArray(int arr[], int size);
+
+int main() {
+	string filename;
+	cout << "Enter filename: ";
+	getline(cin, filename);
+	if (readFromFile(filename)) {
+		pressEnterToContinue();
+		return 0;
+	}
+	else {
+		pressEnterToContinue();
+		return 1;
+	}
+}
+
+bool readFromFile(string filename) {
+	ifstream inFile;
+	string thisLine;
+
+	int letterCount[26] = { 0 };
+
+	inFile.open(filename);
+
+	if (inFile.fail()) {
+		cout << "read error - sorry\n";
+		return false;
+	}
+
+	while (true) {
+		if (inFile.fail()) break;
+		thisLine.clear();
+		getline(inFile, thisLine);
+
+		for (char c : thisLine) {
+			if (isalpha(c)) {
+				c = tolower(c);
+				letterCount[c - 'a']++;
+			}
+		}
+	}
+
+	double totalLetters = sumArray(letterCount, 26);
+
+	cout << "Total number of letters: " << totalLetters << endl;
+	cout << "Frequency:" << endl;
+
+	for (int i = 0; i < 26; i++) {
+		char letter = 'A' + i;
+		cout << "'" << letter << "': " << fixed << setprecision(3) << letterCount[i]/totalLetters << endl;
+	}
+
+	inFile.close();
+	return true;
+}
+
+void pressEnterToContinue(void) {
+	char c;
+	cout << "Press Enter to continue...";
+	cin.ignore(1024, '\n');
+	do { cin.get(c); } while (c != '\n' && c != EOF);
+	return;
+}
+
+int sumArray(int arr[], int size) {
+	int total = 0;
+	for (int i = 0; i < size; i++) {
+		total += arr[i];
+	}
+	return total;
+}
